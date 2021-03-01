361fcf2e1244dc7590449331a0091a538073a554
Yash-y-ash <wardhan696@gmail.com>
2021-03-02 00:39:15 +0530

    Added:store value in dictionary

--- arithmaticCompute.sh
+++ arithmaticCompute.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 read a b c
-echo $(($a%$b+$c))
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
+
+declare -A dr
+dr[a]=$result1
+dr[b]=$result2
+dr[c]=$result3
+dr[d]=$result4
+
+echo ${dr[@]}
