4c5dbec0e39c0dfa90a603cb2ed9279abc87e43e
Yash-y-ash <wardhan696@gmail.com>
2021-03-02 15:56:00 +0530

    Added:read dictionary in array

--- arithmaticCompute.sh
+++ arithmaticCompute.sh
@@ -6,9 +6,24 @@ result3=$(($c+$a/$b))
 result4=$(($a%$b+$c))
 
 declare -A dr
+
 dr[a]=$result1
 dr[b]=$result2
 dr[c]=$result3
 dr[d]=$result4
 
-echo ${dr[@]}
+
+    VALUES=()
+    count=0
+    for DATA in "${dr[@]}" ; do
+        VALUES+=("${DATA##*:}")
+		  let count++
+    done
+
+		for((i=0;i<count;i++))
+		do
+		   ar[i]=${VALUES[i]}
+      done
+
+		echo="value of dictionary in array is: ${ar[@]}"
+
